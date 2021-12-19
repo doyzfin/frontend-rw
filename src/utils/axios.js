30186f4d0358a0df9349d9a947e69eccfd63d3dd
Alfin <alfinramadhan@majapahit.id>
2021-12-19 09:45:01 +0700

    fix

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosApiIntances = axios.create({
-  baseURL: "http://localhost:3001/backend1/api/v1",
+  baseURL: "https://rw10-app.herokuapp.com/backend1/api/v1",
 });
 // Add a request interceptor
 axiosApiIntances.interceptors.request.use(
